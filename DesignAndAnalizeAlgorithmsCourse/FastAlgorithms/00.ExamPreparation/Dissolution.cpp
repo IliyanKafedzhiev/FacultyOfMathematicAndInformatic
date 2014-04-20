efe69dd9a7021b26a4aba49adf77d30f4f81d727
IliyanKafedzhiev <iliqnkafedjiev@gmial.com>
2014-04-20 22:26:03 +0300

    test

--- DesignAndAnalizeAlgorithmsCourse/FastAlgorithms/00.ExamPreparation/Dissolution.cpp
+++ DesignAndAnalizeAlgorithmsCourse/FastAlgorithms/00.ExamPreparation/Dissolution.cpp
@@ -7,12 +7,89 @@ using namespace std;
 vector<vector<char > > Matrix;
 bool Final = false;
 
-void wave(int i, int j)
+bool HasTwoSolutionSides(int x, int y,char sol)
 {
-
+	int sides = 0;
+	if (x - 1 >= 0 && Matrix[x - 1][y] == sol)
+	{
+		sides++;
+	}
+	if (x + 1 < Matrix.size() && Matrix[x + 1][y] == sol)
+	{
+		sides++;
+	}
+	if (y - 1 >= 0 && Matrix[x][y - 1] == sol)
+	{
+		sides++;
+	}
+	if (y + 1 < Matrix.size() && Matrix[x][y + 1] == sol)
+	{
+		sides++;
+	}
+	if (sides >=2)
+	{
+		return true;
+	}
+	return false;
 }
 
-int mainZ()
+bool HasOneSolutionSideOrBorder(int x, int y)
+{
+	int sides = 0;
+	if (x - 1 >= 0)
+	{
+		if (Matrix[x - 1][y] == '.')
+		{
+			sides++;
+		}
+	}
+	else
+	{
+		sides++;
+	}
+	if (x + 1 < Matrix.size())
+	{
+		if (Matrix[x + 1][y] == '.')
+		{
+			sides++;
+		}
+	}
+	else
+	{
+		sides++;
+	}
+	if (y - 1 >= 0)
+	{
+		if (Matrix[x][y - 1] == '.')
+		{
+			sides++;
+		}
+	}
+	else
+	{
+		sides++;
+	}
+	if (y + 1 < Matrix.size())
+	{
+		if (Matrix[x][y + 1] == '.')
+		{
+			sides++;
+		}
+	}
+	else
+	{
+		sides++;
+	}
+	if (sides >= 1 )
+	{
+		return true;
+	}
+	return false;
+}
+
+
+
+int main()
 {
 	int N, Days = 0; scanf("%d\n", &N); Matrix.resize(N);
 	for (size_t i = 0; i < N; i++)
@@ -33,20 +110,63 @@ int mainZ()
 			}
 		}
 	}
-
+	int Xmin = 0, Xmax = N - 1;
+	int Ymin = 0, Ymax = N - 1;
+	char sol = '.';
 	//wave in
-	for (size_t i = 0; i < N; i++)
+	while (!Final)
 	{
-		for (size_t j = 0; j < N; j++)
+		bool xmin = false;
+		int XXmax = Xmax;
+		int YYmin = 30000000;
+		int YYmax = 0;
+		for (int i = Xmin; i <= Xmax; i++)
 		{
-			if (Matrix[i][j] == '#' && !Final)
+			int YCurMin = 3000;
+			int YcurMax = 0;
+			bool ycurMin = false;
+			for (int j = Ymin; j < Ymax; j++)
 			{
-				wave(i, j);
-				Days++;
+				if (Matrix[i][j] == '#'  && HasTwoSolutionSides(i,j,sol))
+				{
+					Matrix[i][j] = sol + 1;
+					if (!xmin)
+					{
+						Xmin = i;
+						xmin = true;
+					}
+					XXmax = i;
+					if (!ycurMin)
+					{
+						YCurMin = j;
+						ycurMin = true;
+					}
+					YcurMax = j;
+				}
+			}
+			if (ycurMin)
+			{
+				if (YYmin > YCurMin)
+				{
+					YYmin = YCurMin;
+				}
+			}
+			if (YcurMax > YYmax)
+			{
+				YYmax = YcurMax;
 			}
 		}
+		if (YYmin == 30000000)
+		{
+			break;
+		}
+		Ymax = YYmax;
+		Ymin = YYmin;
+		Xmax = XXmax;
+		Days++;
+		sol = sol + 1;
 	}
-	printf("%d\n", Days);
+	
 
 	return 0;
 }
