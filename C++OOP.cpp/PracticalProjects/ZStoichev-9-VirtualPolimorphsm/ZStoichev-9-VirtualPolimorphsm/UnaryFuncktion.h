ccd920470870ddbad8078bae1b4633fcbcd2d473
IliyanKafedzhiev <iliqnkafedjiev@gmial.com>
2014-05-15 07:52:04 +0300

    Naming homework added

--- C++OOP.cpp/PracticalProjects/ZStoichev-9-VirtualPolimorphsm/ZStoichev-9-VirtualPolimorphsm/UnaryFuncktion.h
+++
@@ -1,9 +0,0 @@
-
-class UnaryFunction {
-public:
-  virtual ~UnaryFunction() {}
-  virtual double value(double x) const = 0;
-  virtual UnaryFunction* derivative() const = 0;
-  virtual UnaryFunction* clone() const = 0;
-};
-
