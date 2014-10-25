6494d027f35f3c54dbe6af592af97aa12d84f9d7
IliyanKafedzhiev <iliqnkafedjiev@gmial.com>
2014-10-26 00:24:51 +0300

    Added first project Test STL Librery

---
+++ DesignAndAnalizeAlgorithmsCourse/FastAlgorithms/100.SystemTestContestsPlatforms/STL_Tests.cpp
@@ -0,0 +1,17 @@
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+#include <stdio.h>
+#include <deque>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0;
+}
